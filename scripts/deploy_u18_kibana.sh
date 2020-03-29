25f83cf57fcdcbb5650572a98f0b120fdd722bed
guenter hipler <guenter.hipler@unibas.ch>
2020-03-29 10:09:26 +0200

    deploy kibana u18

---
+++ scripts/deploy_u18_kibana.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+BASE_KIBANA_DIR=/etc/kibana
+
+
+#create data and log dirs
+#sollte nach sb-uelk1 da dort nur logstash -> Paul sagen
+#for host in sb-uelk1
+for host in sb-uelk2 sb-uelk3 sb-uelk4
+
+do
+
+    scp ../kibana/configs/kibana.yml root@${host}:${BASE_KIBANA_DIR}
+    #ssh root@${host} "ufw allow 5601"
+    ssh root@${host} "systemctl restart kibana.service"
+
+done
+
+
